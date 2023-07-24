7d28e4837766af87692625f4dbc458c2ce66eadb
planoff111 <planoff111@gmail.com>
2023-07-24 19:46:34 +0300

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,11 @@ public class Main {
     private static final int NUM_ITERATIONS = 10;
     private static final String CHICKEN = "Chicken";
     private static final String EGG = "Egg";
+    private static final Object lock = new Object();
+    private static String currentPlayer = CHICKEN;
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CustomThread thread1 = new  CustomThread();
+        CustomThread thread1 = new CustomThread();
         thread1.start();
         CustomRunnable customRunnable = new CustomRunnable();
         Thread thread = new Thread(customRunnable);
@@ -17,16 +20,17 @@ public class Main {
         Future task = service.submit(customCallable);
         System.out.println(task.get());
         Thread newThread = new Thread(
-                ()-> System.out.println("I am Thread from lamda" + Thread.currentThread().getName())
+                () -> System.out.println("I am Thread from lamda" + Thread.currentThread().getName())
         );
         newThread.start();
-
         startThreadsForInc();
     }
-    static  int  value = 0;
-// так виніс за межі методу змінну результат не консистентний кожен раз при додаванні join то інкрементує по черзі
+
+    static int value = 0;
+
+    // так виніс за межі методу змінну результат не консистентний кожен раз при додаванні join то інкрементує по черзі
     static void startThreadsForInc() throws InterruptedException {
-        for (int i = 0; i < 10 ; i++){
+        for (int i = 0; i < 10; i++) {
             Thread threadForInc = new Thread(
                     () -> {
                         value++;
@@ -60,14 +64,33 @@ public class Main {
 
     private static void playGame(String player) {
         for (int i = 0; i < NUM_ITERATIONS; i++) {
-            System.out.println(player);
+            synchronized (lock) {
+                while (!player.equals(currentPlayer)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                System.out.println(player);
+
+                if (player.equals(CHICKEN)) {
+                    currentPlayer = EGG;
+                } else {
+                    currentPlayer = CHICKEN;
+                }
+
+                lock.notifyAll();
+            }
+
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-    }
 
+    }
 }
 
